247b2dbd0dccd818dde5f613b1880dac5757e13c
Cameron Ibrahim <dev@cameronibrahim.com>
2022-07-20 23:25:01 -0400

    Added iterative version

---
+++ MDTest_iter.jl
@@ -0,0 +1,51 @@
+using Coarsening, Graphs, LinearOrdering, MatrixMarket, BenchmarkTools
+
+include("./itertreewidth.jl")
+include("./dynamictreewidth.jl")
+
+fname = "./graphs/regular3_32_2_0.mtx" #192x192
+#fname = "./graphs/regular5_32_4_2.mtx" #256x256
+
+### New Graphs (Weighted Undirected) ###
+#fname = "./graphs/Plants_10NN.mtx" #stackoverflow error 1600x1600
+#fname = "./graphs/Binaryalphadigs_10NN.mtx" #Jupyter Kerenel keeps dying 1404x1404
+#fname = "./graphs/collins_15NN.mtx" #Jupyter Kernel keeps dying 1000x1000
+#fname = "./graphs/Vehicle_10NN.mtx" #Jupyer Kernel keeps dying 846x846
+#fname = "./graphs/Ecoli_10NN.mtx" #stackoverflow error 336x336
+#fname = "./graphs/YaleA_10NN.mtx" #stackoverdlow error 165x165
+
+adj = makeadj(mmread(fname))
+
+G = SimpleGraph(adj);
+
+config = (
+            compat_sweeps=10,
+            stride_percent=0.5,
+            gauss_sweeps=10,
+            coarsening=VolumeCoarsening(0.4, 2.0, 5),
+            coarsest=10,
+            pad_percent=0.05,
+            node_window_sweeps=10,
+            node_window_size=1,
+            seed = 0
+)
+onesum = PSum(1)
+
+# I think the use of global variables might mess with performance, but whatever for rn
+function profileme()
+    position_to_idx, idx_to_position = ordergraph(onesum, G; config...);
+    pmap = PositionMap(position_to_idx, idx_to_position)
+    onesumval = LinearOrdering.evalorder(onesum, adjacency_matrix(G), idx_to_position)
+    cost, _ = iter_width(adjacency_matrix(G), pmap)[1, length(position_to_idx)]
+end
+
+profileme() # Do not profile - for precompilation
+print("Number of threads: ")  
+println(Threads.nthreads())
+print("Graph used: ") 
+println(fname)
+@btime profileme() # Profile this
+
+println()
+println()
+
---
+++ itertreewidth.jl
@@ -0,0 +1,93 @@
+using SparseArrays
+using Random
+using LinearAlgebra
+using DataStructures
+
+struct PositionMap
+    p_to_v::Vector{Int} # position to vertex
+    v_to_p::Vector{Int} # vertex to position
+end
+
+function _init_table!(table, A, pmap)
+    fill!(table, (0.0, 1))
+    vals = nonzeros(A)
+    for p in size(table, 1)
+        acc = 0.0
+        for idx in nzrange(A, pmap.p_to_v[p])
+            acc += vals[idx]
+        end
+        table[p] = (acc, p)
+    end
+    return table
+end
+
+# Costs
+tsize(outgoing, _) = outgoing
+tops(outgoing, shared) = outgoing + shared
+
+# Merge
+log2sumexp2(x, y) = max(x, y) + log1p(exp2(min(x, y) - max(x, y))) / log(2)
+
+function weighted_degree(A, pmap, i, k, j)
+    rows = rowvals(A)
+    vals = nonzeros(A)
+    left = right = out = zero(eltype(A))
+    v = pmap.p_to_v[k]
+    for idx in nzrange(A, v)
+        r = pmap.v_to_p[rows[idx]]
+        if !(i <= r <= j)
+            out += vals[idx]
+        elseif i <= r < k
+            left += vals[idx]
+        elseif k < r <= j
+            right += vals[idx]
+        end
+    end
+    return out, left, right
+end
+
+function calc_vals!(between, A, pmap, i, j) # GPU parallelization on this might be helpful/useful
+    outgoing = zero(eltype(A))
+    outgoing, _, between[i] = weighted_degree(A, pmap, i, i, j)
+    for k in (i + 1):j
+        out, left, right = weighted_degree(A, pmap, i, k, j)
+        outgoing += out
+        between[k] = between[k - 1] + right - left
+    end
+    return outgoing, between
+end
+
+function iter_width(A, pmap; cost=tsize, merge=max)
+    n = size(A, 1)
+    table = fill((0.0, 0), (n, n))
+    return _iter_width!(table, A, pmap; cost=cost, merge=merge)
+end
+
+function _iter_width!(table, A, pmap; cost=tsize, merge=max)
+    _init_table!(table, A, pmap)
+    n = length(pmap.p_to_v)
+    between = zeros(n)
+
+    for win_size in 2:n
+        for i in 1:(n-win_size+1)
+            j = i + win_size - 1
+            outgoing, between = calc_vals!(between, A, pmap, i, j)
+            best = Inf
+            bestk = -1
+            for k in i:(j-1)
+                v = pmap.p_to_v[k]
+                b = between[k]
+
+                c = cost(outgoing, b)
+                c = merge(merge(c, table[i, k][1]), table[k+1, j][1])
+                if c < best
+                    best = c
+                    bestk = k
+                end
+            end
+            table[i, j] = (best, bestk)
+        end
+    end
+    return table
+end
+
